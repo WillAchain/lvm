1511459593df943be8a431ac968ff23083481da8
xiaoming <xiaoming@new4g.cn>
2017-12-26 15:44:05 +0800

    delete unused code

--- main.cpp
+++ main.cpp
@@ -9,26 +9,12 @@
 
 bool g_stub_test_enable = false;
 
-void rolloutHandler(const char* filename, std::size_t size) {
-    static int idx = 0;
-    std::stringstream stream;
-    stream << filename << "." << ++idx << ".log";
-    fc::rename(filename, stream.str().c_str());
-}
-
-void log_init() {
-}
-
-void log_close() {
-}
-
 int main(int argc, char** argv) {
     if (g_stub_test_enable) {
         StubPtr stub = std::make_shared<Stub>();
         stub->start();
         return 0;
     }
-    log_close();
     try {
         ClientPtr client = std::make_shared<Client>();
         client->configure_from_command_line(argc, argv);
